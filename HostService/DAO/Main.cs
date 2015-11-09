31aab3241789aa3a2985bce809d7b9781a769161
Thomas Ritter <masterwow3@web.de>
2015-11-09 22:06:47 +0100

    Adding Session Factory.

---
+++ HostService/DAO/Main.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAO.User;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate;
+
+namespace DAO
+{
+    public class Main
+    {
+        private static ISessionFactory CreateSessionFactory()
+        {
+            return Fluently.Configure()
+              .Database(MySQLConfiguration.Standard)
+              .Mappings(m =>
+                m.FluentMappings
+                  .AddFromAssemblyOf<UserMaps>())
+              .BuildSessionFactory();
+        }
+    }
+}
