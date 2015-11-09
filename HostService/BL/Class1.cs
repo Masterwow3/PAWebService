7a8c0873e455f6563c87f1553b651308ae120f88
Thomas Ritter <masterwow3@web.de>
2015-11-09 22:14:57 +0100

    do something.

--- HostService/BL/Class1.cs
+++ HostService/BL/Class1.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Entities.User;
 
 namespace BL
 {
     public class Class1
     {
+        User tset = new User();
     }
 }
